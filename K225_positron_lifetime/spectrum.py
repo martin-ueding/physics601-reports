d405203d4ab98c075060b4b49e5bd8dbcfa04933
Martin Ueding <dev@martin-ueding.de>
2016-03-31 21:44:08 +0200

    Refactor code

--- K225_positron_lifetime/spectrum.py
+++ K225_positron_lifetime/spectrum.py
@@ -5,6 +5,7 @@ Functions for the expected lifetime spectrum.
 '''
 
 import glob
+import itertools
 
 import numpy as np
 import scipy.optimize as op
@@ -17,52 +18,38 @@ import models
 def job_lifetime_spectra(T):
     files = glob.glob('Data/in-*.txt')
 
-    for i in range(len(files)):
-        data = np.loadtxt(files[i])
-        channel = data[:,0]
-        counts = data[:,1]
-
-        mean = []
-        width = []
-        A_0 = []
-        A_t = []
-        tau_0 = []
-        tau_t = []
-        BG = []
-
-        for a in range(2):
-            boot_counts = bootstrap.redraw_count(counts)
-            popt, pconv = op.curve_fit(models.lifetime_spectrum, channel, boot_counts, p0=[
-                1600,
-                45,
-                180,
-                180,
-                40,
-                40,
-                0
-                ])
-            mean.append(popt[0])
-            width.append(popt[1])
-            A_0.append(popt[2])
-            A_t.append(popt[3])
-            tau_0.append(popt[4])
-            tau_t.append(popt[5])
-            BG.append(popt[6])
-
-        mean_val, mean_err = bootstrap.average_and_std_arrays(mean)
-        width_val, width_err = bootstrap.average_and_std_arrays(width)
-        A_0_val, A_0_err = bootstrap.average_and_std_arrays(A_0)
-        A_t_val, A_t_err = bootstrap.average_and_std_arrays(A_t)
-        tau_0_val, tau_0_err = bootstrap.average_and_std_arrays(tau_0)
-        tau_t_val, tau_t_err = bootstrap.average_and_std_arrays(tau_t)
-        BG_val, BG_err = bootstrap.average_and_std_arrays(BG)
-
-
     x = np.linspace(1000, 3000, 500)
-    y = models.lifetime_spectrum(x, mean_val, width_val, A_0_val, A_t_val, tau_0_val, tau_t_val, BG_val)
 
-    pl.plot(channel, counts, linestyle="none", marker="o")
-    pl.plot(x, y)
-    dandify_plot()
-    pl.savefig('_build/mpl-channel-counts.pdf')
-    pl.clf()
+    for i, file_ in zip(itertools.count(), files):
+        print('Working on lifetime spectrum', file_)
+
+        data = np.loadtxt(file_)
+        channel = data[:, 0]
+        counts = data[:, 1]
+
+        results = []
+        for a in range(conf.SAMPLES):
+            boot_counts = bootstrap.redraw_count(counts)
+            results.append(_lifetime_kernel(channel, boot_counts, x))
+        popt_dist, y_dist = zip(*results)
+
+        popt_val, popt_err = bootstrap.average_and_std_arrays(popt_dist)
+        mean_val, width_val, A_0_val, A_t_val, tau_0_val, tau_t_val, bg_val = popt_val
+        mean_err, width_err, A_0_err, A_t_err, tau_0_err, tau_t_err, bg_err = popt_err
+
+        y_val, y_err = bootstrap.average_and_std_arrays(y_dist)
+
+        np.savetxt('_build/xy/lifetime-{}-data.tsv'.format(i),
+                   np.column_stack((channel, counts)))
+        np.savetxt('_build/xy/lifetime-{}-fit.tsv'.format(i),
+                   np.column_stack((x, y_val)))
+        np.savetxt('_build/xy/lifetime-{}-band.tsv'.format(i),
+                   bootstrap.pgfplots_error_band(x, y_val, y_err))
+
+
+
+def _lifetime_kernel(channel, counts, x):
+    p0 = [1600, 45, 180, 180, 40, 40, 0]
+    popt, pconv = op.curve_fit(models.lifetime_spectrum, channel, counts, p0=p0)
+    y = models.lifetime_spectrum(x, *popt)
+    return popt, y
