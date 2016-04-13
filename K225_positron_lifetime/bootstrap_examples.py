a93117d0d01d15a120cca58ad513489db8eb599d
Martin Ueding <dev@martin-ueding.de>
2016-04-13 13:36:47 +0200

    Start with bootstrap examples

---
+++ K225_positron_lifetime/bootstrap_examples.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Copyright © 2016 Martin Ueding <dev@martin-ueding.de>
+
+import argparse
+import random
+
+import matplotlib.pyplot as pl
+import numpy as np
+import scipy.optimize as op
+
+import bootstrap
+
+
+def linear(x, a, b):
+    return a * x + b
+
+
+def main():
+    options = _parse_args()
+
+    NUM_POINTS = 7
+
+    # x = np.linspace(1, 5, NUM_POINTS)
+    # errs = [np.abs(random.gauss(1, 0.5)) for X in x]
+    # y = [random.gauss(X, err) for X, err in zip(x, errs)]
+
+    # np.savetxt('example.txt',
+    #            np.column_stack([x, y, errs]))
+
+    data = np.loadtxt('example.txt')
+    x = data[:, 0]
+    y = data[:, 1]
+    y_err = data[:, 2]
+
+    popt, pconv = op.curve_fit(linear, x, y, sigma=y_err)
+
+    fit_x = np.linspace(np.min(x), np.max(x), 100)
+    fit_y = linear(fit_x, *popt)
+
+    pl.errorbar(x, y, yerr=y_err, linestyle='none', marker='+')
+    pl.margins(0.05)
+    pl.plot(fit_x, fit_y)
+    pl.savefig('raw.pdf')
+    pl.clf()
+
+    y_dist = []
+
+    for i in range(10):
+        boot_y = [random.gauss(Y, err) for Y, err in zip(y, y_err)]
+        popt, pconv = op.curve_fit(linear, x, boot_y)
+        boot_fit_y = linear(fit_x, *popt)
+
+        pl.plot(x, boot_y, linestyle='none', marker='+')
+        pl.plot(fit_x, boot_fit_y)
+
+        y_dist.append(boot_fit_y)
+
+    pl.margins(0.05)
+    pl.savefig('boot.pdf')
+    pl.clf()
+
+    fit_y_val, fit_y_err = bootstrap.average_and_std_arrays(y_dist)
+
+    pl.errorbar(x, y, yerr=y_err, linestyle='none', marker='+')
+    pl.plot(fit_x, fit_y_val)
+    pl.fill_between(fit_x, fit_y_val + fit_y_err, fit_y_val - fit_y_err)
+    pl.margins(0.05)
+    pl.savefig('averaged.pdf')
+    pl.clf()
+
+
+
+
+def _parse_args():
+    '''
+    Parses the command line arguments.
+
+    :return: Namespace with arguments.
+    :rtype: Namespace
+    '''
+    parser = argparse.ArgumentParser(description='')
+    options = parser.parse_args()
+
+    return options
+
+
+if __name__ == '__main__':
+    main()
