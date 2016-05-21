6b54b3dcffa2595178798bf4f49b88573a9cafc6
Martin Ueding <dev@martin-ueding.de>
2016-05-21 20:51:18 +0200

    Implement Rayleigh length

--- A245_doubling/crunch.py
+++ A245_doubling/crunch.py
@@ -43,6 +43,55 @@ def cos_squared(x, ampl, x_offs, y_offs):
     return ampl * (np.cos(x - x_offs))**2 + y_offs
 
 
+def get_rayleigh_length(radius, wavelength, refractive_index, distance):
+    numerator = refractive_index * radius**2 - np.sqrt(refractive_index**2 * radius**4 - 4 * wavelength**2 * distance**2)
+    return np.pi * numerator / (2 * wavelength)
+
+
+def get_waist(rayleigh_length, wavelength, refractive_index):
+    return np.sqrt(rayleigh_length * wavelength / (refractive_index * np.pi))
+
+
+def task_rayleigh_length(T):
+    beam_diameter_val = 3.5e-3
+    beam_diameter_err = 0.5e-3
+    refractive_index = 2.2
+    wavelength = 987e-9
+    length = 5e-3
+    distance = 60e-3
+
+    beam_radius_val = beam_diameter_val / 2
+    beam_radius_err = beam_diameter_err / 2
+
+    T['beam_radius'] = siunitx(beam_radius_val, beam_radius_err)
+
+    beam_radius_dist = bootstrap.make_dist(beam_radius_val, beam_diameter_err)
+
+    rayleigh_length_dist = list(itertools.filterfalse(np.isnan, [
+        get_rayleigh_length(beam_radius, wavelength, refractive_index, distance)
+        for beam_radius in beam_radius_dist
+    ]))
+    rayleigh_length_val, rayleigh_length_err = bootstrap.average_and_std_arrays(rayleigh_length_dist)
+    T['rayleigh_length'] = siunitx(rayleigh_length_val, rayleigh_length_err)
+    print(rayleigh_length_dist)
+
+    waist_dist = list(itertools.filterfalse(np.isnan, [
+        get_waist(rayleigh_length, wavelength, refractive_index)
+        for rayleigh_length in rayleigh_length_dist
+    ]))
+    waist_val, waist_err = bootstrap.average_and_std_arrays(waist_dist)
+    T['waist'] = siunitx(waist_val, waist_err)
+    print(waist_dist)
+
+    normalized_length_dist = list([
+        length / (2 * rayleigh_length)
+        for rayleigh_length in rayleigh_length_dist
+    ])
+    normalized_length_val, normalized_length_err = bootstrap.average_and_std_arrays(normalized_length_dist)
+    T['normalized_length'] = siunitx(normalized_length_val, normalized_length_err)
+    print(normalized_length_dist)
+
+
 def test_keys(T):
     '''
     Testet das dict auf Schlüssel mit Bindestrichen.
@@ -74,6 +123,8 @@ def main():
 
     parser = argparse.ArgumentParser()
     options = parser.parse_args()
+
+    task_rayleigh_length(T)
 
     test_keys(T)
     with open('_build/template.js', 'w') as f:
