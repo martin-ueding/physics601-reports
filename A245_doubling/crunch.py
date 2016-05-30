162a5e3622fda1e93848c473c0b5efc7fd379c2e
Martin Ueding <dev@martin-ueding.de>
2016-05-30 18:24:15 +0200

    Insert refractive index

--- A245_doubling/crunch.py
+++ A245_doubling/crunch.py
@@ -130,15 +130,6 @@ def job_power(T):
     return extinction_dist
 
 
-def get_rayleigh_length(radius, wavelength, refractive_index, distance):
-    numerator = np.pi * refractive_index * radius**2 - np.sqrt(np.pi**2 * refractive_index**2 * radius**4 - 4 * wavelength**2 * distance**2)
-    return numerator / (2 * wavelength)
-
-
-def get_waist(rayleigh_length, wavelength, refractive_index):
-    return np.sqrt(rayleigh_length * wavelength / (refractive_index * np.pi))
-
-
 def get_optimal_focal_length(beam_radius, refractive_index, wavelength, length):
     optimal_normalized_length = 2.84
     bracket = 2 * beam_radius**2 * refractive_index * np.pi * optimal_normalized_length / (length * wavelength) - 1
@@ -149,7 +140,7 @@ def get_optimal_focal_length(beam_radius, refractive_index, wavelength, length):
 def job_rayleigh_length(T):
     beam_diameter_val = 3.5e-3
     beam_diameter_err = 0.5e-3
-    refractive_index = 1 # 2.2
+    refractive_index = 2.2
     wavelength = 987e-9
     length = 5e-3
     distance = 60e-3
@@ -176,7 +167,7 @@ def job_rayleigh_length(T):
     T['waist_mum'] = siunitx(waist_val / 1e-6, waist_err / 1e-6)
 
     rayleigh_length_dist = list(itertools.filterfalse(np.isnan, [
-        np.pi * waist**2 / wavelength
+        refractive_index * np.pi * waist**2 / wavelength
         for waist in waist_dist
     ]))
     rayleigh_length_val, rayleigh_length_err = bootstrap.average_and_std_arrays(rayleigh_length_dist)
